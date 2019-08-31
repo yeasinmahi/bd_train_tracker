df6cf6698a3a0f2972e1e5ceb3b6001784c1ac7a
yeasinmahi <yeasinmahi72@yahoo.com>
2019-08-31 13:25:11 +0600

    update gradle

--- app/src/main/java/com/gits/arafat/traintracker/Activity/SentSms.java
+++ app/src/main/java/com/gits/arafat/traintracker/Activity/SentSms.java
@@ -9,7 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+import androidx.core.app.ActivityCompat;
 import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.view.View;
--- app/src/main/java/com/gits/arafat/traintracker/Others/Utility.java
+++ app/src/main/java/com/gits/arafat/traintracker/Others/Utility.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Build;
-import android.support.v4.content.ContextCompat;
+import androidx.core.content.ContextCompat;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
